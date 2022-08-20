168ecc4b796c59b9c93fcfd292b092447fb330ae
mudasir ahmed <mudasirahmed572@gmail.com>
2022-08-20 15:58:37 +0500

    modified port

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 const allRoutes = require('./router/routes')
 
-let port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'))
